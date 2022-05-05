ab4b60d6dfb14c924de0713988606362de866b9e
aidn3 <aidn.david3@gmail.com>
2022-05-05 19:48:34 +0200

    add /calculate to minecraft commands

---
+++ src/minecraft/commands/CalculateCommand.js
@@ -0,0 +1,14 @@
+const {evalExpression} = require('@hkh12/node-calc');
+
+module.exports = {
+    triggers: ['calculate', 'calc', 'c'],
+    handler: function (clientInstance, reply, username, ...args) {
+        if (args.length === 0) {
+            reply(`${username}, example: /calc 1 + 1`)
+            return
+        }
+        let expression = args.join(" ")
+        let result = evalExpression(expression)
+        reply(`${username}, ${expression} = ${result}`)
+    }
+}
