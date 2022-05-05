7bb7f1961b5f4e6c5414b699f0a80aa2672338bf
aidn3 <aidn.david3@gmail.com>
2022-05-05 19:41:29 +0200

    add /weight to minecraft commands

---
+++ src/minecraft/commands/WeightCommand.js
@@ -0,0 +1,40 @@
+const HYPIXEL_KEY = process.env.HYPIXEL_KEY
+const fetch = require("axios");
+
+module.exports = {
+    triggers: ['weight', 'w'],
+    handler: async function (clientInstance, reply, username, ...args) {
+
+        let givenUsername = args[0] !== undefined ? args[0] : username
+        let uuid = await getUuidByUsername(givenUsername)
+
+        if (!uuid) {
+            reply(`No such username! (given: ${givenUsername})`)
+            return
+        }
+
+        let senither = await getSenitherData(uuid)
+        let weight = senither?.weight
+        if (!weight) {
+            reply(`${username} doesn't play Skyblock?`)
+            return
+        }
+
+        reply(`${givenUsername}'s weight: ${Math.floor(weight)}`)
+    }
+}
+
+function getSenitherData(uuid) {
+    return fetch(`https://hypixel-api.senither.com/v1/profiles/${uuid}/weight?key=${HYPIXEL_KEY}`)
+        .then(resource => resource.data)
+        .then(response => response?.data)
+        .catch(e => console.log(`Error retrieving ${uuid}. Error: ${e.message}`))
+        .finally(() => {
+            return {}
+        })
+}
+
+function getUuidByUsername(username) {
+    return fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`)
+        .then(res => res.data?.id)
+}
