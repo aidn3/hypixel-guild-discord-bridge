461883e1eb5e34f25a6e4ce86f0efe62e6ece4fb
aidn3 <aidn.david3@gmail.com>
2022-05-07 10:34:02 +0200

    fix spelling in iq message

--- src/minecraft/commands/IqCommand.js
+++ src/minecraft/commands/IqCommand.js
@@ -2,6 +2,6 @@ module.exports = {
     triggers: ['iq'],
     handler: function (clientInstance, reply, username, args) {
         let givenUsername = args[0] !== undefined ? args[0] : username
-        reply(`${givenUsername} has IQ of ${Math.floor(Math.random() * 200)}`)
+        reply(`${givenUsername} has an IQ of ${Math.floor(Math.random() * 200)}`)
     }
 }
