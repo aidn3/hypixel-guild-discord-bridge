435a51d69a592dd21f83f6207b764374b8aad77b
aidn3 <aidn.david3@gmail.com>
2022-05-07 03:17:17 +0200

    add async to prevent crashing in case of an unknown math expression

--- src/minecraft/commands/CalculateCommand.js
+++ src/minecraft/commands/CalculateCommand.js
@@ -2,7 +2,7 @@ const {evalExpression} = require('@hkh12/node-calc');
 
 module.exports = {
     triggers: ['calculate', 'calc', 'c'],
-    handler: function (clientInstance, reply, username, args) {
+    handler: async function (clientInstance, reply, username, args) {
         if (args.length === 0) {
             reply(`${username}, example: /calc 1 + 1`)
             return
