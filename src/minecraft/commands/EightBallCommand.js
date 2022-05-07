1398725c59b96e25f0689a92e431b226523120a3
aidn3 <aidn.david3@gmail.com>
2022-05-07 10:31:35 +0200

    Add /8ball command to ingame

---
+++ src/minecraft/commands/EightBallCommand.js
@@ -0,0 +1,29 @@
+const ANSWERS = [
+    "As I see it, yes.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don’t count on it.",
+    "It is certain.",
+    "It is decidedly so.",
+    "Most likely.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Outlook good.",
+    "Reply hazy, try again.",
+    "Signs point to yes.",
+    "Very doubtful.",
+    "Without a doubt.",
+    "Yes.",
+    "Yes – definitely.",
+    "You may rely on it."
+]
+
+module.exports = {
+    triggers: ['8balls', '8ball', '8', 'ball', 'balls', '8b'],
+    handler: function (clientInstance, reply, username, args) {
+        reply(`${username}, ${ANSWERS[Math.floor(Math.random() * ANSWERS.length)]}`)
+    }
+}
