11d37a729c156577ca4b5c176c2739da9872aa2e
aidn3 <aidn.david3@gmail.com>
2022-05-10 03:57:50 +0200

    add lowestbin command to ingame chat

---
+++ src/minecraft/commands/LowestbinCommand.js
@@ -0,0 +1,69 @@
+/*
+ CREDIT: Idea by Aura
+ Discord: Aura#5051
+ Minecraft username: _aura
+*/
+const fetch = require("axios");
+const {Index} = require("flexsearch");
+
+module.exports = {
+    triggers: ['lowestbin', 'lbin', 'lb'],
+    handler: async function (clientInstance, reply, username, args) {
+
+        if (args.length === 0) {
+            reply(`${username}, at least give an item name.`)
+            return
+        }
+
+        let item = args.join(" ")
+        let foundItem = await findItem(item)
+
+        if (!foundItem) {
+            reply(`${username}, item does not exists. Try to be more specific maybe?`)
+            return
+        }
+
+        reply(`${foundItem.name}'s lbin is ${localizedPrice(foundItem.lowestBin)}`)
+    }
+}
+
+let index = new Index()
+let cache
+let lastRetrieve = 0
+
+async function refreshCache() {
+    if (!cache || lastRetrieve + 300 * 1000 < new Date().getTime()) {
+        let data = await fetch('https://maro.skybrokers.xyz/api/auctions/all')
+            .then(res => res.data.data)
+
+        for (let i = 0; i < data.length; i++) {
+            index.add(i, data[i].id)
+            index.append(i, data[i].name)
+        }
+
+        cache = data
+    }
+
+    console.log(`cached ${cache.length}`)
+}
+
+async function findItem(search) {
+    await refreshCache()
+
+    let result = index.search(search, 1)
+    if (result.length >= 0) return cache[result[0]]
+}
+
+function localizedPrice(coins) {
+    let suffix = ""
+    if (coins > 1000) {
+        coins = coins / 1000
+        suffix = "k"
+    }
+    if (coins > 1000) {
+        coins = coins / 1000
+        suffix = "m"
+    }
+
+    return parseFloat(coins.toFixed(3)) + suffix
+}
