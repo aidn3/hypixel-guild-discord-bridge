17b05a852426d9edd84252f6866a83b3732e752a
aidn3 <aidn.david3@gmail.com>
2022-05-18 15:06:52 +0200

    fix escaping an escape character.
    input: cloud_beautiful
    expected: cloud\_beautiful
    output:cloud\\_beautiful
    discord ouput: "cloud\_beautiful"
    expected after fix: "cloud_beautiful"

--- src/common/DiscordMessageUtil.js
+++ src/common/DiscordMessageUtil.js
@@ -52,8 +52,8 @@ function cleanMessage(messageEvent) {
 const escapeDiscord = function (message) {
     if (!message) return ""
 
-    message = message.split('_').join('\\_') // Italic
     message = message.split('\\').join('\\\\') // "\"
+    message = message.split('_').join('\\_') // Italic
     message = message.split('*').join('\\*') // bold
     message = message.split('~').join('\\~') // strikethrough
     message = message.split('`').join('\\`') // code
