80fb897cf8cbcea167a19257aa4afae1e320d244
aidn3 <aidn.david3@gmail.com>
2022-09-20 23:55:08 +0200

    fix bug: new time mute isn't applied if it is shorter

--- src/util/MutedUsers.js
+++ src/util/MutedUsers.js
@@ -2,19 +2,27 @@ class PunishedUsers {
     #muted = [];
 
     muted(name) {
-        let current = new Date().getTime()
+        this.#clean()
 
-        this.#muted = this.#muted.filter(p => p.till > current)
+        let current = new Date().getTime()
         return this.#muted.find(p => p.name.toLowerCase() === name.toLowerCase() && p.till > current)
     }
 
     mute(name, time) {
+        this.#clean()
+        this.unmute(name)
+
         let current = new Date().getTime()
         this.#muted.push({name: name, till: current + time})
     }
 
     unmute(name) {
-        this.#muted = this.#muted.filter(p => p.name !== name)
+        this.#muted = this.#muted.filter(p => p.name.toLowerCase() !== name.toLowerCase())
+    }
+
+    #clean() {
+        let current = new Date().getTime()
+        this.#muted = this.#muted.filter(p => p.till > current)
     }
 }
 
