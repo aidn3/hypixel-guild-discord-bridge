b2c5fe460102da15e6a9223549d55fb4c82677b0
aidn5 <aydin.atrakchi@gmail.com>
2022-10-21 21:28:03 +0200

    Bugfix: minecraft Kick notification was triggered on people sending a fake kick chat message

--- src/minecraft/chat/KickChat.js
+++ src/minecraft/chat/KickChat.js
@@ -2,7 +2,7 @@ const {SCOPE} = require("../../common/ClientInstance")
 const COLOR = require('../../../config/discord-config.json').events.color
 
 module.exports = function (clientInstance, message) {
-    let regex = /(?:\[[A-Z+]{1,10}\] ){0,3}(\w{3,32}) was kicked from the guild by .{1,32}!$/g
+    let regex = /^(?:\[[A-Z+]{1,10}\] ){0,3}(\w{3,32}) was kicked from the guild by .{1,32}!$/g
 
     let match = regex.exec(message)
     if (match != null) {
