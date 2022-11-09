8132ccb4218d4b9c465252a300b416c5f2a52c99
aidn5 <aydin.atrakchi@gmail.com>
2022-11-09 18:27:13 +0100

    feature: Add in-game event player request to join guild

---
+++ src/minecraft/chat/RequestChat.js
@@ -0,0 +1,22 @@
+const {SCOPE} = require("../../common/ClientInstance")
+const COLOR = require('../../../config/discord-config.json').events.color
+
+module.exports = function (clientInstance, message) {
+    let regex = /^-{53}\n\[[A-Za-z+]{3,10}\] {0,3}(\w{3,32}) has requested to join the Guild/g
+
+    let match = regex.exec(message)
+    if (match != null) {
+        let username = match[1]
+
+        clientInstance.app.emit("minecraft.event.request", {
+            clientInstance: clientInstance,
+            scope: SCOPE.PUBLIC,
+            username: username,
+            severity: COLOR.GOOD,
+            message: message,
+            removeLater: false
+        })
+
+        return true
+    }
+}
