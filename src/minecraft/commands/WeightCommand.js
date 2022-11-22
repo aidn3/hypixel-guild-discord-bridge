50d968f73b749ad6d22a11bdac984a08b065d2bc
aidn5 <aydin.atrakchi@gmail.com>
2022-11-22 22:28:30 +0100

    bugfix: fix weight command not working due to hypixel "last_saved" profile change

--- src/minecraft/commands/WeightCommand.js
+++ src/minecraft/commands/WeightCommand.js
@@ -3,40 +3,31 @@
  Discord: Aura#5051
  Minecraft username: _aura
 */
-const HYPIXEL_KEY = process.env.HYPIXEL_KEY
-const axios = require("axios");
+const axios = require("axios")
 
 module.exports = {
     triggers: ['weight', 'w'],
     handler: async function (clientInstance, username, args) {
 
         let givenUsername = args[0] !== undefined ? args[0] : username
-        let uuid = await getUuidByUsername(givenUsername)
-
-        if (!uuid) {
-            return `No such username! (given: ${givenUsername})`
-        }
-
-        return `${givenUsername}'s weight: ${await getSenitherData(uuid)}`
+        return `${givenUsername}'s weight: ${await getSenitherData(givenUsername)}`
     }
 }
 
-async function getSenitherData(uuid) {
-    let res = await axios(`https://hypixel-api.senither.com/v1/profiles/${uuid}/weight?key=${HYPIXEL_KEY}`)
-        .then(res => res.data.data)
+async function getSenitherData(username) {
+    let profiles = await axios(`https://sky.shiiyu.moe/api/v2/profile/${username}`)
+        .then(res => res.data["profiles"])
 
-    let total = (res?.weight || 0) + (res?.weight_overflow || 0)
-    let skills = (res?.skills?.weight || 0) + (res?.skills?.weight_overflow || 0)
-    let slayers = (res?.slayers?.weight || 0) + (res?.slayers?.weight_overflow || 0)
-    let dungeons = (res?.dungeons?.weight || 0) + (res?.dungeons?.weight_overflow || 0)
+    let weight = Object.values(profiles)
+        .find(profile => profile["current"])["data"]["weight"]["senither"]
+
+    let total = (weight.overall || 0)
+    let skills = (weight.skill.total || 0)
+    let slayers = (weight.slayer.total || 0)
+    let dungeons = (weight.dungeon.total || 0)
 
     return `${Math.floor(total)}`
         + ` / Skills ${Math.floor(skills)} (${((skills / total) * 100).toFixed(1)}%)`
         + ` / Slayers ${Math.floor(slayers)} (${((slayers / total) * 100).toFixed(1)}%)`
         + ` / Dungeons ${Math.floor(dungeons)} (${((dungeons / total) * 100).toFixed(1)}%)`
-}
-
-function getUuidByUsername(username) {
-    return axios(`https://api.mojang.com/users/profiles/minecraft/${username}`)
-        .then(res => res.data?.id)
 }
