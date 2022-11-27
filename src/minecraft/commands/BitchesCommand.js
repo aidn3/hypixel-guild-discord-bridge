87e142029d757c00f49d9ae3f07b73c8b83d4b6e
aidn5 <aydin.atrakchi@gmail.com>
2022-11-27 22:03:06 +0100

    improvement: In-game Bitches command now says "b" instead of "B"

--- src/minecraft/commands/BitchesCommand.js
+++ src/minecraft/commands/BitchesCommand.js
@@ -7,6 +7,6 @@ module.exports = {
     triggers: ['bitch', 'bitches', 'b'],
     handler: function (clientInstance, username, args) {
         let givenUsername = args[0] !== undefined ? args[0] : username
-        return `${givenUsername} has ${Math.floor(Math.random() * 10)} B'`
+        return `${givenUsername} has ${Math.floor(Math.random() * 10)} b'`
     }
 }
