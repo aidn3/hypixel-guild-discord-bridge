8827fc9a67874cf5f03d10e0b4cf1ad4c748e220
aidn5 <aydin.atrakchi@gmail.com>
2022-12-17 21:30:23 +0100

    bugfix: minecraft instance has no email and password to login

--- src/instance/minecraft/common/MinecraftConfig.ts
+++ src/instance/minecraft/common/MinecraftConfig.ts
@@ -1,6 +1,9 @@
 import MineFlayer = require('mineflayer')
 
 export default interface MinecraftConfig {
+    email: string
+    password: string
+
     // @ts-ignore
     botOptions: { client: MineFlayer.Client } & Partial<MineFlayer.BotOptions>
     bridgePrefix: string
