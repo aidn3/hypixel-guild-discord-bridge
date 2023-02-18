7e93340190058b0a2a6025085f99c40c1ded9672
aidn5 <aydin.atrakchi@gmail.com>
2023-02-18 07:23:05 +0100

    fix #31 Blocked message event repeats itself nonstop Discord have less strict language control by default and isn't as moderated as Minecraft servers. Minecraft instance doesn't need to broadcast blocked messages to begin with. They are only needed for discord instance to warn users about their language. If a message will be blocked by a minecraft instance, then another minecraft instance can not possibly send that message to begin with. Hence, blocking it completely is an optimal fix.

--- src/instance/minecraft/MinecraftInstance.ts
+++ src/instance/minecraft/MinecraftInstance.ts
@@ -59,6 +59,7 @@ export default class MinecraftInstance extends ClientInstance<MinecraftConfig> {
             if (event.scope !== SCOPE.PUBLIC) return
             if (event.removeLater) return
             if (event.name === EventType.COMMAND) return
+            if (event.name === EventType.BLOCK) return
 
             return this.send(`/gc @[${event.instanceName || "Main"}]: ${event.message}`)
         })
