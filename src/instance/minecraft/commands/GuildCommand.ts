4627fe29cc192023eb35c7794401ef7f55585363
aidn5 <aydin.atrakchi@gmail.com>
2023-06-14 06:43:39 +0200

    make GuildCommand.ts more compact for #58

--- src/instance/minecraft/commands/GuildCommand.ts
+++ src/instance/minecraft/commands/GuildCommand.ts
@@ -24,20 +24,11 @@ export default <MinecraftCommandMessage>{
             return `No such username! (given: ${givenUsername})`
         }
 
-        return `${givenUsername}'s guild: ${await fetchGuildInfo(clientInstance.app.hypixelApi, uuid)}`
+
+        let guild = await clientInstance.app.hypixelApi.getGuild("player", uuid, {})
+        if (!guild) return `${givenUsername} is not in any guild.`
+
+        let member = guild.members.find((m: { uuid: string }) => m.uuid === uuid)
+        return `${givenUsername} in ${guild.name} (${guild.members.length}/125) as ${member?.rank}`
     }
-}
-
-async function fetchGuildInfo(hypixel: Client, uuid: string) {
-    let guild = await hypixel.getGuild("player", uuid, {})
-
-    if (!guild) return "No Guild."
-
-    let member = guild.members.find((m: { uuid: string }) => m.uuid === uuid)
-    return `Name: ${guild.name}`
-        + ` / Level: ${guild.level}`
-        + ` / Created: ${moment(guild.createdAtTimestamp).format('YYYY-MM-DD')}`
-        + ` / Members: ${guild.members.length}/125`
-        + ` / Rank: ${member?.rank}`
-        + ` / Joined: ${moment(member?.joinedAtTimestamp).format('YYYY-MM-DD')}`
 }
