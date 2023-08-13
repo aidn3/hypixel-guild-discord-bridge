8098dfe1e8b0ccaf8574acb6cfe3f0bd9f0bbbcd
aidn3 <aidn.david3@gmail.com>
2023-08-13 02:17:53 +0200

    Eslint fix application files

--- src/Application.ts
+++ src/Application.ts
@@ -1,213 +1,217 @@
-import fs = require("fs");
-import {TypedEmitter} from 'tiny-typed-emitter'
-import {Client as HypixelClient} from 'hypixel-api-reborn'
-import DiscordInstance from "./instance/discord/DiscordInstance"
-import MinecraftInstance from "./instance/minecraft/MinecraftInstance"
-import GlobalChatInstance from "./instance/globalChat/GlobalChatInstance"
-import WebhookInstance from "./instance/webhook/WebhookInstance"
-import {PunishedUsers} from "./util/PunishedUsers"
+import fs = require('fs')
+import { TypedEmitter } from 'tiny-typed-emitter'
+import { Client as HypixelClient } from 'hypixel-api-reborn'
+import DiscordInstance from './instance/discord/DiscordInstance'
+import MinecraftInstance from './instance/minecraft/MinecraftInstance'
+import GlobalChatInstance from './instance/globalChat/GlobalChatInstance'
+import WebhookInstance from './instance/webhook/WebhookInstance'
+import { PunishedUsers } from './util/PunishedUsers'
 import {
-    ChatEvent,
-    ClientEvent,
-    CommandEvent,
-    InstanceEvent,
-    InstanceRestartSignal,
-    InstanceSelfBroadcast,
-    MinecraftCommandResponse,
-    MinecraftRawChatEvent,
-    MinecraftSelfBroadcast,
-    MinecraftSendChat,
-    ShutdownSignal
-} from "./common/ApplicationEvent"
-import {ClientInstance} from "./common/ClientInstance"
-import PluginInterface from "./common/PluginInterface"
-import MetricsInstance from "./instance/metrics/MetricsInstance"
-import ClusterHelper from "./ClusterHelper"
-import * as Events from "events";
-import {getLogger, Logger} from "log4js";
-import {ApplicationConfig} from "./ApplicationConfig";
-import SocketInstance from "./instance/socket/SocketInstance";
-import * as path from "path";
-
+  ChatEvent,
+  ClientEvent,
+  CommandEvent,
+  InstanceEvent,
+  InstanceRestartSignal,
+  InstanceSelfBroadcast,
+  MinecraftCommandResponse,
+  MinecraftRawChatEvent,
+  MinecraftSelfBroadcast,
+  MinecraftSendChat,
+  ShutdownSignal
+} from './common/ApplicationEvent'
+import { ClientInstance } from './common/ClientInstance'
+import PluginInterface from './common/PluginInterface'
+import MetricsInstance from './instance/metrics/MetricsInstance'
+import ClusterHelper from './ClusterHelper'
+import * as Events from 'events'
+import { getLogger, Logger } from 'log4js'
+import { ApplicationConfig } from './ApplicationConfig'
+import SocketInstance from './instance/socket/SocketInstance'
+import * as path from 'path'
 
 export default class Application extends TypedEmitter<ApplicationEvents> {
+  private readonly logger: Logger
+  private readonly instances: Array<ClientInstance<any>> = []
+  private readonly plugins: PluginInterface[] = []
 
-    private readonly logger: Logger
-    private readonly instances: ClientInstance<any>[] = []
-    private readonly plugins: PluginInterface[] = []
+  readonly clusterHelper: ClusterHelper
+  readonly punishedUsers: PunishedUsers
+  readonly hypixelApi: HypixelClient
+  readonly config: ApplicationConfig
 
-    readonly clusterHelper: ClusterHelper
-    readonly punishedUsers: PunishedUsers
-    readonly hypixelApi: HypixelClient
-    readonly config: ApplicationConfig
+  constructor (config: ApplicationConfig) {
+    super()
+    this.logger = getLogger('Application')
+    this.logger.trace('Application initiating')
+    emitAll(this) // first thing to redirect all events
+    this.config = config
 
-    constructor(config: ApplicationConfig) {
-        super()
-        this.logger = getLogger("Application")
-        this.logger.trace("Application initiating")
-        emitAll(this) // first thing to redirect all events
-        this.config = config
+    this.hypixelApi = new HypixelClient(this.config.general.hypixelApiKey, {
+      cache: true,
+      cacheTime: 300
+    })
+    this.punishedUsers = new PunishedUsers()
+    this.clusterHelper = new ClusterHelper(this)
 
-        this.hypixelApi = new HypixelClient(this.config.general.hypixelApiKey, {cache: true, cacheTime: 300})
-        this.punishedUsers = new PunishedUsers()
-        this.clusterHelper = new ClusterHelper(this)
+    let discordInstance: DiscordInstance | null = null
+    if (this.config.discord.key != null) {
+      discordInstance = new DiscordInstance(this, this.config.discord.instanceName, this.config.discord)
+      this.instances.push(discordInstance)
+    }
 
-        let discordInstance: DiscordInstance | null = null
-        if (this.config.discord.key) {
-            discordInstance = new DiscordInstance(this, this.config.discord.instanceName, this.config.discord)
-            this.instances.push(discordInstance)
-        }
+    for (const instanceConfig of this.config.webhooks) {
+      this.instances.push(new WebhookInstance(this, instanceConfig.instanceName, discordInstance != null ? discordInstance.client : null, instanceConfig))
+    }
 
-        for (let instanceConfig of this.config.webhooks) {
-            this.instances.push(new WebhookInstance(this, instanceConfig.instanceName, discordInstance != null ? discordInstance.client : null, instanceConfig))
-        }
+    if (this.config.global.enabled) {
+      this.instances.push(new GlobalChatInstance(this, this.config.global.instanceName, this.config.global))
+    }
 
-        if (this.config.global.enabled) {
-            this.instances.push(new GlobalChatInstance(this, this.config.global.instanceName, this.config.global))
-        }
+    for (const instanceConfig of this.config.minecrafts) {
+      this.instances.push(new MinecraftInstance(this, instanceConfig.instanceName, instanceConfig))
+    }
 
-        for (let instanceConfig of this.config.minecrafts) {
-            this.instances.push(new MinecraftInstance(this, instanceConfig.instanceName, instanceConfig))
-        }
+    if (this.config.metrics.enabled) {
+      this.instances.push(new MetricsInstance(this, this.config.metrics.instanceName, this.config.metrics))
+    }
 
-        if (this.config.metrics.enabled) {
-            this.instances.push(new MetricsInstance(this, this.config.metrics.instanceName, this.config.metrics))
-        }
+    if (this.config.socket.enabled) {
+      this.instances.push(new SocketInstance(this, this.config.socket.instanceName, this.config.socket))
+    }
 
-        if (this.config.socket.enabled) {
-            this.instances.push(new SocketInstance(this, this.config.socket.instanceName, this.config.socket))
-        }
+    if (this.config.plugins.enabled) {
+      const mainPath = require.main?.path ?? process.cwd()
+      this.logger.debug(`Loading plugins with main path as: ${mainPath}`)
+      let paths
 
-        if (this.config.plugins.enabled) {
-            const mainPath = require.main?.path ?? process.cwd()
-            this.logger.debug(`Loading plugins with main path as: ${mainPath}`)
-            let paths
+      if (this.config.plugins.paths === undefined) {
+        this.logger.warn('Plugins config is old. Resolving default plugins. See config_example.yaml for the latest config scheme')
+        paths = fs.readdirSync('./src/plugins/')
+          .filter(file => file.endsWith('Plugin.ts'))
+          .map(f => path.resolve(mainPath, 'src/plugins', f))
+      } else {
+        paths = this.config.plugins.paths.map(p => path.isAbsolute(p) ? p : path.resolve(mainPath, p))
+      }
 
-            if (this.config.plugins.paths === undefined) {
-                this.logger.warn('Plugins config is old. Resolving default plugins. See config_example.yaml for the latest config scheme')
-                paths = fs.readdirSync('./src/plugins/')
-                    .filter(file => file.endsWith('Plugin.ts'))
-                    .map(f => path.resolve(mainPath, 'src/plugins', f))
-            } else {
-                paths = this.config.plugins.paths.map(p => path.isAbsolute(p) ? p : path.resolve(mainPath, p))
-            }
+      this.plugins = paths.map(f => {
+        this.logger.debug(`Loading Plugin ${path.relative(mainPath, f)}`)
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        return require(f).default
+      })
+    } else {
+      this.plugins = []
+    }
 
-            this.plugins = paths.map(f => {
-                this.logger.debug(`Loading Plugin ${path.relative(mainPath, f)}`)
-                return require(f).default
-            })
-        } else {
-            this.plugins = []
-        }
+    this.on('shutdownSignal', (event) => {
+      if (event.targetInstanceName === null) {
+        this.logger.info('Shutdown signal has been received. Shutting down this node.')
+        this.logger.info('Node should auto restart if a process monitor service is used.')
+        this.logger.info('Waiting 5 seconds for other nodes to receive the signal before shutting down.')
 
-        this.on('shutdownSignal', async (event) => {
-            if (event.targetInstanceName === null) {
-                this.logger.info('Shutdown signal has been received. Shutting down this node.')
-                this.logger.info('Node should auto restart if a process monitor service is used.')
-                this.logger.info('Waiting 5 seconds for other nodes to receive the signal before shutting down.')
-                await new Promise(r => setTimeout(r, 5000))
-                process.exit(2)
-            }
+        void new Promise(resolve => setTimeout(resolve, 5000)).then(() => {
+          process.exit(2)
         })
+      }
+    })
+  }
+
+  async sendConnectSignal (): Promise<void> {
+    this.broadcastLocalInstances()
+
+    // only shared with plugins to directly modify instances
+    // everything else is encapsulated
+    const getLocalInstance = (instanceName: string): ClientInstance<any> | undefined => {
+      return this.instances.find(i => i.instanceName === instanceName)
     }
 
-    async sendConnectSignal() {
-        this.broadcastLocalInstances()
+    this.logger.debug('Sending signal to all plugins')
+    this.plugins.forEach(p => p.onRun(this, getLocalInstance))
 
-        // only shared with plugins to directly modify instances
-        // everything else is encapsulated
-        let getLocalInstance = (instanceName: string): ClientInstance<any> | undefined => {
-            return this.instances.find(i => i.instanceName === instanceName)
-        }
-
-        this.logger.debug("Sending signal to all plugins")
-        this.plugins.forEach(p => p.onRun(this, getLocalInstance))
-
-        for (let instance of this.instances) {
-            this.logger.debug(`Connecting instance ${instance.instanceName}`)
-            await instance.connect()
-        }
+    for (const instance of this.instances) {
+      this.logger.debug(`Connecting instance ${instance.instanceName}`)
+      await instance.connect()
     }
+  }
 
-    public broadcastLocalInstances(): void {
-        this.logger.debug("Informing instances of each other")
+  public broadcastLocalInstances (): void {
+    this.logger.debug('Informing instances of each other')
 
-        for (let instance of this.instances) {
-            this.emit("selfBroadcast", {
-                localEvent: true,
-                instanceName: instance.instanceName,
-                location: instance.location
-            })
-        }
+    for (const instance of this.instances) {
+      this.emit('selfBroadcast', {
+        localEvent: true,
+        instanceName: instance.instanceName,
+        location: instance.location
+      })
     }
+  }
 }
 
-function emitAll(emitter: Events): void {
-    let old = emitter.emit
-    emitter.emit = (event: string, ...args: Parameters<any>): boolean => {
-        if (event !== "*") emitter.emit("*", event, ...args)
-        return old.call(emitter, event, ...args)
-    }
+function emitAll (emitter: Events): void {
+  const old = emitter.emit
+  emitter.emit = (event: string, ...args: Parameters<any>): boolean => {
+    if (event !== '*') emitter.emit('*', event, ...args)
+    return old.call(emitter, event, ...args)
+  }
 }
 
 export interface ApplicationEvents {
-    /**
-     * Receive all events
-     * @param event event name
-     * @param args event arguments
-     */
-    '*': (event: string, ...args: any) => void
+  /**
+   * Receive all events
+   * @param event event name
+   * @param args event arguments
+   */
+  '*': (event: string, ...args: any) => void
 
-    /**
-     * User sending messages
-     */
-    'chat': (event: ChatEvent) => void
-    /**
-     * User join/leave/offline/online/mute/kick/etc
-     */
-    'event': (event: ClientEvent) => void
-    /**
-     * User executing a command
-     */
-    'command': (event: CommandEvent) => void
-    /**
-     * Internal instance start/connect/disconnect/etc
-     */
-    'instance': (event: InstanceEvent) => void
+  /**
+   * User sending messages
+   */
+  'chat': (event: ChatEvent) => void
+  /**
+   * User join/leave/offline/online/mute/kick/etc
+   */
+  'event': (event: ClientEvent) => void
+  /**
+   * User executing a command
+   */
+  'command': (event: CommandEvent) => void
+  /**
+   * Internal instance start/connect/disconnect/etc
+   */
+  'instance': (event: InstanceEvent) => void
 
-    /**
-     * Broadcast instance to inform other applications nodes in cluster about its existence
-     */
-    'selfBroadcast': (event: InstanceSelfBroadcast) => void
-    /**
-     * Command used to restart an instance.
-     * Note: This is currently only registered in Minecraft instances
-     */
-    'restartSignal': (event: InstanceRestartSignal) => void
+  /**
+   * Broadcast instance to inform other applications nodes in cluster about its existence
+   */
+  'selfBroadcast': (event: InstanceSelfBroadcast) => void
+  /**
+   * Command used to restart an instance.
+   * Note: This is currently only registered in Minecraft instances
+   */
+  'restartSignal': (event: InstanceRestartSignal) => void
 
-    /**
-     * Command used to shut down the bridge.
-     * It will take some time for the bridge to shut down.
-     * Bridge will auto restart if a service monitor is used.
-     */
-    'shutdownSignal': (event: ShutdownSignal) => void
+  /**
+   * Command used to shut down the bridge.
+   * It will take some time for the bridge to shut down.
+   * Bridge will auto restart if a service monitor is used.
+   */
+  'shutdownSignal': (event: ShutdownSignal) => void
 
-    /**
-     * Used to broadcast which in-game username/uuid belongs to which bot.
-     * Useful to distinguish in-game between players and bots
-     */
-    'minecraftSelfBroadcast': (event: MinecraftSelfBroadcast) => void
-    /**
-     * Minecraft instance raw chat
-     */
-    'minecraftChat': (event: MinecraftRawChatEvent) => void
-    /**
-     * Command used to send a chat message/command through a minecraft instance
-     */
-    'minecraftSend': (event: MinecraftSendChat) => void
+  /**
+   * Used to broadcast which in-game username/uuid belongs to which bot.
+   * Useful to distinguish in-game between players and bots
+   */
+  'minecraftSelfBroadcast': (event: MinecraftSelfBroadcast) => void
+  /**
+   * Minecraft instance raw chat
+   */
+  'minecraftChat': (event: MinecraftRawChatEvent) => void
+  /**
+   * Command used to send a chat message/command through a minecraft instance
+   */
+  'minecraftSend': (event: MinecraftSendChat) => void
 
-    /**
-     * Response of an executed command from minecraft instance
-     */
-    'minecraftCommandResponse': (event: MinecraftCommandResponse) => void
+  /**
+   * Response of an executed command from minecraft instance
+   */
+  'minecraftCommandResponse': (event: MinecraftCommandResponse) => void
 }
--- src/ClusterHelper.ts
+++ src/ClusterHelper.ts
@@ -1,74 +1,74 @@
-import Application from "./Application"
-import {MinecraftSelfBroadcast} from "./common/ApplicationEvent"
-import {LOCATION} from "./common/ClientInstance"
+import Application from './Application'
+import { MinecraftSelfBroadcast } from './common/ApplicationEvent'
+import { LOCATION } from './common/ClientInstance'
 
 export default class ClusterHelper {
-    private readonly app: Application
-    private readonly minecraftBots = new Map<string, MinecraftSelfBroadcast>()
-    private readonly instancesNames = new Map<LOCATION, Set<string>>()
+  private readonly app: Application
+  private readonly minecraftBots = new Map<string, MinecraftSelfBroadcast>()
+  private readonly instancesNames = new Map<LOCATION, Set<string>>()
 
-    constructor(app: Application) {
-        this.app = app
+  constructor (app: Application) {
+    this.app = app
 
-        this.app.on("minecraftSelfBroadcast", event => this.minecraftBots.set(event.instanceName, event))
-        this.app.on("instance", event => this.instanceBroadcast(event.instanceName, event.location))
-        this.app.on("selfBroadcast", event => this.instanceBroadcast(event.instanceName, event.location))
+    this.app.on('minecraftSelfBroadcast', event => this.minecraftBots.set(event.instanceName, event))
+    this.app.on('instance', event => { this.instanceBroadcast(event.instanceName, event.location) })
+    this.app.on('selfBroadcast', event => { this.instanceBroadcast(event.instanceName, event.location) })
+  }
+
+  sendCommandToMinecraft (instanceName: string, command: string): void {
+    this.app.emit('minecraftSend', {
+      localEvent: true,
+      targetInstanceName: instanceName,
+      command
+    })
+  }
+
+  sendCommandToAllMinecraft (command: string): void {
+    this.app.emit('minecraftSend', {
+      localEvent: true,
+      targetInstanceName: null,
+      command
+    })
+  }
+
+  getMinecraftBotsUuid (): string[] {
+    const uuids: string[] = []
+    this.minecraftBots.forEach((v) => uuids.push(v.uuid))
+    return uuids
+  }
+
+  getInstancesNames (location: LOCATION): string[] {
+    const instanceNames = this.instancesNames.get(location)
+    if (instanceNames == null) return []
+
+    const result: string[] = []
+    for (const instanceName of instanceNames) {
+      result.push(instanceName)
+    }
+    return result
+  }
+
+  isMinecraftBot (username: string): boolean {
+    for (const value of this.minecraftBots.values()) {
+      if (username === value.username) return true
     }
 
-    sendCommandToMinecraft(instanceName: string, command: string): void {
-        this.app.emit("minecraftSend", {
-            localEvent: true,
-            targetInstanceName: instanceName,
-            command: command
-        })
+    return false
+  }
+
+  private instanceBroadcast (instanceName: string, location: LOCATION): void {
+    let collection = this.instancesNames.get(location)
+    if (collection == null) {
+      collection = new Set<string>()
+      this.instancesNames.set(location, collection)
     }
+    collection.add(instanceName)
+  }
 
-    sendCommandToAllMinecraft(command: string): void {
-        this.app.emit("minecraftSend", {
-            localEvent: true,
-            targetInstanceName: null,
-            command: command
-        })
-    }
-
-    getMinecraftBotsUuid(): string[] {
-        let uuids: string[] = []
-        this.minecraftBots.forEach((v) => uuids.push(v.uuid))
-        return uuids
-    }
-
-    getInstancesNames(location: LOCATION): string[] {
-        let instanceNames = this.instancesNames.get(location)
-        if (!instanceNames) return []
-
-        let result: string[] = []
-        for (let instanceName of instanceNames) {
-            result.push(instanceName)
-        }
-        return result
-    }
-
-    isMinecraftBot(username: string): boolean {
-        for (let value of this.minecraftBots.values()) {
-            if (username === value.username) return true
-        }
-
-        return false
-    }
-
-    private instanceBroadcast(instanceName: string, location: LOCATION): void {
-        let collection = this.instancesNames.get(location)
-        if (!collection) {
-            collection = new Set<string>()
-            this.instancesNames.set(location, collection)
-        }
-        collection.add(instanceName)
-    }
-
-    /**
+  /**
      * @deprecated
      */
-    getHypixelApiKey(): string {
-        return this.app.hypixelApi.key
-    }
+  getHypixelApiKey (): string {
+    return this.app.hypixelApi.key
+  }
 }
