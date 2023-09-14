a63007d2fd2a245f70338532055c3e27aac0a35c
Hx <94485261+HyxonQz@users.noreply.github.com>
2023-09-14 21:23:28 +0100

    Add username prefixes

--- src/instance/minecraft/commands/RouletteCommand.ts
+++ src/instance/minecraft/commands/RouletteCommand.ts
@@ -2,21 +2,21 @@ import { MinecraftCommandMessage } from '../common/ChatInterface'
 import MinecraftInstance from '../MinecraftInstance'
 
 const LossMessages = [
-  'you got blasted!',
-  'unlucky, wrong choice.',
-  "it's not rigged, I promise!",
-  'you got capped.',
-  'enjoy the mute, haha!',
-  'better luck next time. Or not...'
+  '%s you got blasted!',
+  '%s unlucky, wrong choice.',
+  "%s it's not rigged, I promise!",
+  '%s you got capped.',
+  '%s enjoy the mute, haha!',
+  '%s better luck next time. Or not...'
 ]
 
 const WinMessages = [
-  'you survived?!',
-  'lucky. Do it again!',
-  'alive? shame.',
-  "I'll get you next time",
-  'perhaps i forgot to load it?',
-  "you're crazy. Again again again!"
+  '%s you survived?!',
+  '%s lucky. Do it again!',
+  '%s alive? shame.',
+  "%s I'll get you next time",
+  '%s perhaps i forgot to load it?',
+  "%s you're crazy. Again again again!"
 ]
 
 export default {
@@ -27,7 +27,7 @@ export default {
     const choice = Number(args[0])
 
     if (choice < 1 || choice > 6) {
-      return `a revolver only has 6 bullets.`
+      return `${username}, a revolver only has 6 bullets.`
     }
 
     const random = Math.floor(Math.random() * 6 + 1)
@@ -36,9 +36,9 @@ export default {
       await clientInstance.send(`/gc mute ${username} 15m`)
       clientInstance.app.punishedUsers.mute(username, 900)
 
-      return LossMessages[Math.floor(Math.random() * LossMessages.length)]
+      return LossMessages[Math.floor(Math.random() * LossMessages.length)].replaceAll('%s', username as string)
     }
 
-    return WinMessages[Math.floor(Math.random() * WinMessages.length)]
+    return WinMessages[Math.floor(Math.random() * WinMessages.length)].replaceAll('%s', username as string)
   }
 } satisfies MinecraftCommandMessage
