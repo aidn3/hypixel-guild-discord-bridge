c531e00ea2e5f1acc2f4ba4d7e45244ae698c9b5
aidn3 <aidn.david3@gmail.com>
2023-09-16 00:38:00 +0200

    enforce error type instead of using "any"

--- src/instance/minecraft/handlers/ErrorHandler.ts
+++ src/instance/minecraft/handlers/ErrorHandler.ts
@@ -3,19 +3,19 @@ import MinecraftInstance from '../MinecraftInstance'
 
 export default class StateHandler extends EventHandler<MinecraftInstance> {
   registerEvents(): void {
-    this.clientInstance.client?.on('error', (err: any) => {
+    this.clientInstance.client?.on('error', (err: Error) => {
       this.onError(err)
     })
   }
 
-  private onError(error: { code: string }): void {
+  private onError(error: Error & { code?: string }): void {
     this.clientInstance.logger.error('Minecraft Bot Error: ', error)
-    if (error?.code === 'EAI_AGAIN') {
+    if (error.code === 'EAI_AGAIN') {
       this.clientInstance.logger.error(
         'Minecraft Bot disconnected duo to internet problems. restarting client in 30 second...'
       )
       setTimeout(() => {
-        void this.clientInstance.connect()
+        this.clientInstance.connect()
       }, 30000)
     }
   }
