fea8a135eac423b722a2924ef0f9a7653422e333
aidn3 <aidn.david3@gmail.com>
2023-09-16 15:54:31 +0200

    Minecraft NetworthCommand.ts now includes museum value

--- src/instance/minecraft/commands/NetworthCommand.ts
+++ src/instance/minecraft/commands/NetworthCommand.ts
@@ -6,7 +6,8 @@
 import { ChatCommandContext, ChatCommandHandler } from '../common/ChatInterface'
 
 import { getNetworth, localizedNetworth } from '../../../util/SkyblockApi'
-import { HypixelSkyblock } from '../../../type/HypixelApiType'
+import { HypixelSkyblock, HypixelSkyblockMuseum } from '../../../type/HypixelApiType'
+import Axios from 'axios'
 
 export default {
   triggers: ['networth', 'net', 'nw'],
@@ -23,12 +24,22 @@ export default {
       return `No such username! (given: ${givenUsername})`
     }
 
-    const networthLocalized = await context.clientInstance.app.hypixelApi
+    const selectedProfile = await context.clientInstance.app.hypixelApi
       .getSkyblockProfiles(uuid, { raw: true })
       .then((response: unknown) => response as HypixelSkyblock)
       .then((res) => res.profiles.filter((p) => p.selected)[0])
-      .then(async (profile) => await getNetworth(profile.members[uuid], profile.banking?.balance ?? 0))
-      .then(localizedNetworth)
+
+    const museumData = await Axios.get(
+      `https://api.hypixel.net/skyblock/museum?key=${context.clientInstance.app.hypixelApi.key}&profile=${selectedProfile.profile_id}`
+    )
+      .then((res) => res.data as unknown as HypixelSkyblockMuseum)
+      .then((museum) => museum.members[uuid])
+
+    const networthLocalized = await getNetworth(
+      selectedProfile.members[uuid],
+      selectedProfile.banking?.balance ?? 0,
+      museumData
+    ).then(localizedNetworth)
 
     return `${givenUsername}'s networth: ${networthLocalized}`
   }
