2d1d6c87ea7185377364016c9b9e073fb791061e
aidn5 <aydin.atrakchi@gmail.com>
2023-09-19 21:42:56 +0200

    Use enum to enforce magic constants. Same literal strings were used in different lines.

--- src/instance/minecraft/commands/RockPaperScissorsCommand.ts
+++ src/instance/minecraft/commands/RockPaperScissorsCommand.ts
@@ -1,15 +1,21 @@
 import { ChatCommandContext, ChatCommandHandler } from '../common/ChatInterface'
 
+enum CommandOptions {
+  ROCK = 'rock',
+  PAPER = 'paper',
+  SCISSORS = 'scissors'
+}
+
 export default {
   triggers: ['rps'],
   enabled: true,
 
   handler: function (context: ChatCommandContext): string {
-    const choices = ['rock', 'paper', 'scissors']
+    const choices = Object.values(CommandOptions)
     const computerChoice = choices[Math.floor(Math.random() * choices.length)]
-    const userChoice = context.args[0]
+    const userChoice = context.args[0] as CommandOptions | undefined
 
-    if (!choices.includes(userChoice)) {
+    if (userChoice == null || !choices.includes(userChoice)) {
       return `${context.username}, choose rock, paper or scissors!`
     }
 
@@ -17,15 +23,15 @@ export default {
       return `${context.username}, I chose ${computerChoice}, We tied!`
     }
 
-    if (userChoice === 'paper' && computerChoice === 'rock') {
+    if (userChoice === CommandOptions.PAPER && computerChoice === CommandOptions.ROCK) {
       return `${context.username}, I chose ${computerChoice}, You won!`
     }
 
-    if (userChoice === 'scissors' && computerChoice === 'paper') {
+    if (userChoice === CommandOptions.SCISSORS && computerChoice === CommandOptions.PAPER) {
       return `${context.username}, I chose ${computerChoice}, You won!`
     }
 
-    if (userChoice === 'rock' && computerChoice === 'scissors') {
+    if (userChoice === CommandOptions.ROCK && computerChoice === CommandOptions.SCISSORS) {
       return `${context.username}, I chose ${computerChoice}, You won!`
     }
 
