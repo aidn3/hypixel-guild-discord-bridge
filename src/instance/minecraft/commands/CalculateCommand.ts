ffe210e6858f456d4109758430c1b24dae7d61a0
Hx <94485261+HyxonQz@users.noreply.github.com>
2023-10-05 16:19:19 +0100

    Added Support For Different Operators Changed Output Format

--- src/instance/minecraft/commands/CalculateCommand.ts
+++ src/instance/minecraft/commands/CalculateCommand.ts
@@ -13,8 +13,13 @@ export default {
   handler: function (context: ChatCommandContext): string {
     if (context.args.length === 0) return `${context.username}, example: !calc 1 + 1`
 
-    const expression = context.args.join(' ')
+    const expression = context.args
+      .join(' ')
+      .replaceAll(':', '/') // division / ratio support
+      .replaceAll('x', '*') // x is also used for multiplication
+      .replaceAll(',', '') // removes commas from numbers
+
     const result = evalExpression(expression)
-    return `${context.username}, ${expression} = ${result}`
+    return `${context.username}, answer: ${result.toLocaleString()}`
   }
 } satisfies ChatCommandHandler
