9551cc23c989a7fbaf855fb3f84afc04e36fec48
Hx <94485261+HyxonQz@users.noreply.github.com>
2023-10-21 14:23:45 +0100

    Added space

--- src/instance/minecraft/CommandsManager.ts
+++ src/instance/minecraft/CommandsManager.ts
@@ -87,7 +87,7 @@ export class CommandsManager extends EventHandler<MinecraftInstance> {
       }
 
       await minecraftInstance.send(
-        `/gc ${command.name}: ${command.description}` +
+        `/gc ${command.name}: ${command.description} ` +
         `(${minecraftInstance.config.commandPrefix}${command.example.replaceAll('%s', username)})`
       )
       return true
