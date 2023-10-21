7cdc68aeef4c787c0e501846030afde2abd3c35d
Hx <94485261+HyxonQz@users.noreply.github.com>
2023-10-21 14:01:00 +0100

    Capitalised Hexes

--- src/instance/discord/common/DiscordConfig.ts
+++ src/instance/discord/common/DiscordConfig.ts
@@ -12,11 +12,11 @@ export interface DiscordConfig {
 }
 
 export enum ColorScheme {
-  GOOD = 0x00_8a_00,
+  GOOD = 0x00_8A_00,
   INFO = 0x84_84_00,
-  BAD = 0x8a_2d_00,
-  ERROR = 0xff_00_00,
-  DEFAULT = 0x09_0a_16
+  BAD = 0x8A_2D_00,
+  ERROR = 0xFF_00_00,
+  DEFAULT = 0x09_0A_16
 }
 
 export const DefaultCommandFooter = 'Made by aidn5 with <3'
