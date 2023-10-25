e1651fb2ce9f65533ce8ec786eca54c2f63f7760
Hx <94485261+HyxonQz@users.noreply.github.com>
2023-10-25 15:44:09 +0100

    Use SkyCrypt to get skill average

--- src/instance/minecraft/commands/SkillCommand.ts
+++ src/instance/minecraft/commands/SkillCommand.ts
@@ -1,5 +1,6 @@
 import * as assert from 'node:assert'
 import { Client, SKYBLOCK_SKILL_DATA, SkyblockMember } from 'hypixel-api-reborn'
+import Axios, { AxiosResponse } from 'axios'
 import { ChatCommandContext, ChatCommandHandler } from '../common/ChatInterface'
 import { formatLevel } from '../../../util/SkyblockApi'
 
@@ -28,12 +29,8 @@ export default {
     const skill = context.args[0]
     const givenUsername = context.args[1] ?? context.username
 
-    if (skill === undefined) {
-      return `Skills: ${Array.from(SKILLS).join(', ')}`
-    }
-
     if (!SKILLS.has(skill)) {
-      return `${context.username}, Invalid skill! (given: ${skill})`
+      return `Skills: ${[...SKILLS].join(', ')}`
     }
 
     const uuid = await context.clientInstance.app.mojangApi
@@ -49,21 +46,12 @@ export default {
 
     const parsedProfile = await getParsedProfile(context.clientInstance.app.hypixelApi, uuid)
 
-    const skills = parsedProfile.skills
-
-
     if (skill === 'average') {
-      const skillAverage = Object.values(skills)
-        .map(skill => {
-          // @ts-expect-error Ignoring impossible to trigger scenario
-          return formatLevel(skill.level, skill.progress)
-        })
-        .reduce((sum, c) => sum + c, 0) / SKILLS.size
-      return `${givenUsername}: ${skill} - ${skillAverage}`
-    }
-    else {
+      const skillAverage = await getSkillAverage(givenUsername)
+      return `${givenUsername}: ${skill} - ${skillAverage.toFixed(2)}`
+    } else {
       // @ts-expect-error Ignoring impossible to trigger scenario
-      const skillData: SKYBLOCK_SKILL_DATA = skills[skill as keyof SkyblockMember['skills']]
+      const skillData: SKYBLOCK_SKILL_DATA = parsedProfile.skills[skill as keyof SkyblockMember['skills']]
       return `${givenUsername}: ${skill} - ${formatLevel(skillData.level, skillData.progress)}`
     }
   }
@@ -81,4 +69,24 @@ async function getParsedProfile(hypixelApi: Client, uuid: string): Promise<Skybl
 
   assert(response)
   return response
+}
+
+async function getSkillAverage(username: string): Promise<number> {
+  const skyShiiyuResponse = await Axios(`https://sky.shiiyu.moe/api/v2/profile/${username}`).then(
+    (response: AxiosResponse) => response.data as SkyShiiyuResponse
+  )
+
+  const selected = Object.values(skyShiiyuResponse.profiles).find((profile) => profile.current)
+  assert(selected)
+
+  return selected.data?.average_level ?? 0
+}
+
+interface SkyShiiyuResponse {
+  profiles: Record<string, SkyShiiyuProfile>
+}
+
+interface SkyShiiyuProfile {
+  current: boolean
+  data?: { average_level: number }
 }
