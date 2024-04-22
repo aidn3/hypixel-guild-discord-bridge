c46134e60e5e2c600d53c3c784f8ac845c71bd60
Greg Mat <65409906+itsvyle@users.noreply.github.com>
2024-04-22 18:05:40 -0400

    Update runstoclassavg.ts
    removed useless stuff

--- src/instance/commands/triggers/runstoclassavg.ts
+++ src/instance/commands/triggers/runstoclassavg.ts
@@ -37,9 +37,7 @@ export default class RunsToClassAvg extends ChatCommandHandler {
     }
     const xpPerRun = FloorsBaseEXP[selectedFloor as keyof typeof FloorsBaseEXP]
     const parsedProfile = await getSelectedSkyblockProfile(context.app.hypixelApi, uuid)
-    // const catacombs = parsedProfile.dungeons.types.catacombs
-    // const skillLevel = this.getLevelWithOverflow(catacombs.xp, catacombs.level, catacombs.progress)
-
+   
     const temporaryClassData = parsedProfile.dungeons.classes
     let temporaryClassAvg = this.getClassAverage(temporaryClassData)
 
@@ -100,39 +98,5 @@ export default class RunsToClassAvg extends ChatCommandHandler {
     } else {
       return Number(level) + progress / 100
     }
-  }
-
-  private formatClass(member: SkyblockMember): string {
-    const classes = member.dungeons.classes
-
-    let xp = 0
-    let level = 0
-    let name = '(None)'
-
-    if (classes.healer.xp > xp) {
-      xp = classes.healer.xp
-      level = this.getLevelWithOverflow(classes.healer.xp, classes.healer.level, classes.healer.progress)
-      name = 'Healer'
-    }
-    if (classes.mage.xp > xp) {
-      xp = classes.mage.xp
-      level = this.getLevelWithOverflow(classes.mage.xp, classes.mage.level, classes.mage.progress)
-      name = 'Mage'
-    }
-    if (classes.berserk.xp > xp) {
-      xp = classes.berserk.xp
-      level = this.getLevelWithOverflow(classes.berserk.xp, classes.berserk.level, classes.berserk.progress)
-      name = 'Berserk'
-    }
-    if (classes.archer.xp > xp) {
-      xp = classes.archer.xp
-      level = this.getLevelWithOverflow(classes.archer.xp, classes.archer.level, classes.archer.progress)
-      name = 'Archer'
-    }
-    if (classes.tank.xp > xp) {
-      level = this.getLevelWithOverflow(classes.tank.xp, classes.tank.level, classes.tank.progress)
-      name = 'Tank'
-    }
-    return `${name} ${level.toFixed(2)}`
   }
 }
