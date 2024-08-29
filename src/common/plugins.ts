e3be6e6b38968dceb8d421e25cca076f147ce5d8
aidn5 <aidn.david3@gmail.com>
2024-08-29 02:46:04 +0200

    chores(lint): enforce import/no-restricted-paths
    This is useful for future contributors to know what not to import.
    This will ensure the code base stays clean and not become a spaghetti mess.

--- src/common/plugins.ts
+++ src/common/plugins.ts
@@ -1,7 +1,10 @@
 import type { Logger } from 'log4js'
 
 import type Application from '../application.js'
+// TODO: fix by either creating special rule in linting or by removing this functionality to meet the standard
+// eslint-disable-next-line import/no-restricted-paths
 import type { ChatCommandHandler } from '../instance/commands/common/command-interface.js'
+// eslint-disable-next-line import/no-restricted-paths
 import type { CommandInterface } from '../instance/discord/common/command-interface.js'
 
 import type { ClientInstance } from './client-instance.js'
