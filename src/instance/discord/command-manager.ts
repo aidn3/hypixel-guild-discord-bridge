aidn3/hypixel-guild-discord-bridge

commit 53baa9c5e4a352c2f853de9df62758ddf48b2eb2

refactor(discord)!: use enums for discord command instance options
boolean was used and only meant "Optional" or "Disabled". enums will allow "Required" which is more precise now

src/instance/discord/command-manager.ts
@@ -6,14 +6,14 @@ import type {
   Interaction,
   RESTPostAPIChatInputApplicationCommandsJSONBody
 } from 'discord.js'
-import { Collection, REST, Routes } from 'discord.js'
+import { Collection, escapeMarkdown, REST, Routes } from 'discord.js'
 import type { Logger } from 'log4js'
 
 import type { DiscordConfig } from '../../application-config.js'
 import type Application from '../../application.js'
-import { ChannelType, InstanceType } from '../../common/application-event.js'
+import { ChannelType, Color, InstanceType } from '../../common/application-event.js'
 import type { DiscordAutoCompleteContext, DiscordCommandContext, DiscordCommandHandler } from '../../common/commands.js'
-import { Permission } from '../../common/commands.js'
+import { OptionToAddMinecraftInstances, Permission } from '../../common/commands.js'
 import EventHandler from '../../common/event-handler.js'
 import type UnexpectedErrorHandler from '../../common/unexpected-error-handler.js'
 import type EventHelper from '../../util/event-helper.js'
@@ -32,6 +32,7 @@ import PunishmentsCommand from './commands/punishments.js'
 import ReconnectCommand from './commands/reconnect.js'
 import RestartCommand from './commands/restart.js'
 import SetrankCommand from './commands/setrank.js'
+import { DefaultCommandFooter } from './common/discord-config.js'
 import type DiscordInstance from './discord-instance.js'
 
 export class CommandManager extends EventHandler<DiscordInstance, InstanceType.Discord> {
@@ -161,7 +162,36 @@ export class CommandManager extends EventHandler<DiscordInstance, InstanceType.D
           ephemeral: true
         })
         return
-      } else if (this.memberAllowed(interaction, command.permission)) {
+      } else if (!this.memberAllowed(interaction, command.permission)) {
+        this.logger.debug('No permission to execute this command')
+
+        await interaction.reply({
+          content: "You don't have permission to execute this command",
+          ephemeral: true
+        })
+        return
+      } else if (
+        (command.addMinecraftInstancesToOptions === OptionToAddMinecraftInstances.Required ||
+          command.addMinecraftInstancesToOptions === OptionToAddMinecraftInstances.Optional) &&
+        this.application.clusterHelper.getInstancesNames(InstanceType.Minecraft).length === 0
+      ) {
+        await interaction.editReply({
+          embeds: [
+            {
+              title: `Command ${escapeMarkdown(command.getCommandBuilder().name)}`,
+              description:
+                `No Minecraft instance exist.\n` +
+                'This is a Minecraft command that requires a working Minecraft account connected to the bridge.\n' +
+                `Check the tutorial on how to add a Minecraft account before using this command.`,
+              color: Color.Info,
+              footer: {
+                text: DefaultCommandFooter
+              }
+            }
+          ]
+        })
+        return
+      } else {
         this.logger.debug('execution granted.')
 
         const username = interaction.inCachedGuild() ? interaction.member.displayName : interaction.user.displayName
@@ -206,14 +236,6 @@ export class CommandManager extends EventHandler<DiscordInstance, InstanceType.D
         }
 
         await command.handler(commandContext)
-        return
-      } else {
-        this.logger.debug('No permission to execute this command')
-
-        await interaction.reply({
-          content: "You don't have permission to execute this command",
-          ephemeral: true
-        })
         return
       }
     } catch (error) {
@@ -290,13 +312,29 @@ export class CommandManager extends EventHandler<DiscordInstance, InstanceType.D
 
     for (const command of this.commands.values()) {
       const commandBuilder = command.getCommandBuilder()
-      if (command.allowInstance && instanceChoices.length > 0) {
-        commandBuilder.addStringOption((option) =>
-          option
-            .setName('instance')
-            .setDescription('Which instance to send this command to')
-            .setChoices(...instanceChoices)
-        )
+      const instanceCommandName = 'instance'
+      const instanceCommandDescription = 'Which instance to send this command to'
+      if (instanceChoices.length > 0) {
+        switch (command.addMinecraftInstancesToOptions) {
+          case OptionToAddMinecraftInstances.Required: {
+            commandBuilder.addStringOption((option) =>
+              option
+                .setName(instanceCommandName)
+                .setDescription(instanceCommandDescription)
+                .setChoices(...instanceChoices)
+                .setRequired(true)
+            )
+            break
+          }
+          case OptionToAddMinecraftInstances.Optional: {
+            commandBuilder.addStringOption((option) =>
+              option
+                .setName(instanceCommandName)
+                .setDescription(instanceCommandDescription)
+                .setChoices(...instanceChoices)
+            )
+          }
+        }
       }
 
       commandsJson.push(commandBuilder.toJSON())

src/common/commands.ts
@@ -61,12 +61,16 @@ export enum Permission {
 
 export interface DiscordCommandHandler {
   getCommandBuilder: () => SlashCommandBuilder
-  // TODO: use enum with "Disabled", "Optional", "Required". Name can be "addMinecraftInstancesToOption"
-  // Maybe rename it "options" and enum like "OptionalMinecraftInstances" | etc
-  allowInstance: boolean
+  addMinecraftInstancesToOptions: OptionToAddMinecraftInstances
   permission: Permission
   handler: (context: DiscordCommandContext) => Promise<void>
   autoComplete?: (context: DiscordAutoCompleteContext) => Promise<void>
+}
+
+export enum OptionToAddMinecraftInstances {
+  Disabled,
+  Optional,
+  Required
 }
 
 interface DiscordContext {

src/instance/discord/commands/about.ts
@@ -1,13 +1,13 @@
 import { SlashCommandBuilder } from 'discord.js'
 
 import type { DiscordCommandHandler } from '../../../common/commands.js'
-import { Permission } from '../../../common/commands.js'
+import { OptionToAddMinecraftInstances, Permission } from '../../../common/commands.js'
 
 export default {
   getCommandBuilder: () =>
     new SlashCommandBuilder().setName('about').setDescription('Display basic info about the client.'),
   permission: Permission.Anyone,
-  allowInstance: false,
+  addMinecraftInstancesToOptions: OptionToAddMinecraftInstances.Disabled,
 
   handler: async function (context) {
     await context.interaction.deferReply()

src/instance/discord/commands/accept.ts
@@ -1,7 +1,7 @@
 import { escapeMarkdown, SlashCommandBuilder } from 'discord.js'
 
 import type { DiscordCommandHandler } from '../../../common/commands.js'
-import { Permission } from '../../../common/commands.js'
+import { OptionToAddMinecraftInstances, Permission } from '../../../common/commands.js'
 import { checkChatTriggers, formatChatTriggerResponse, InviteAcceptChat } from '../common/chat-triggers.js'
 
 export default {
@@ -12,7 +12,7 @@ export default {
       .addStringOption((option) =>
         option.setName('username').setDescription('Username of the player').setRequired(true).setAutocomplete(true)
       ) as SlashCommandBuilder,
-  allowInstance: true,
+  addMinecraftInstancesToOptions: OptionToAddMinecraftInstances.Required,
   permission: Permission.Helper,
 
   handler: async function (context) {
@@ -21,7 +21,7 @@ export default {
     const username: string = context.interaction.options.getString('username', true)
     const command = `/g accept ${username}`
 
-    const instance: string | undefined = context.interaction.options.getString('instance') ?? undefined
+    const instance: string = context.interaction.options.getString('instance', true)
     const result = await checkChatTriggers(
       context.application,
       context.eventHelper,

src/instance/discord/commands/connectivity.ts
@@ -5,7 +5,7 @@ import type Application from '../../../application.js'
 import type { MinecraftRawChatEvent } from '../../../common/application-event.js'
 import { Color, InstanceType } from '../../../common/application-event.js'
 import type { DiscordCommandHandler } from '../../../common/commands.js'
-import { Permission } from '../../../common/commands.js'
+import { OptionToAddMinecraftInstances, Permission } from '../../../common/commands.js'
 import type EventHelper from '../../../util/event-helper.js'
 import { antiSpamString } from '../../../util/shared-util.js'
 import { DefaultCommandFooter } from '../common/discord-config.js'
@@ -49,7 +49,7 @@ export default {
   getCommandBuilder: () =>
     new SlashCommandBuilder().setName('connectivity').setDescription('Check connectivity to Minecraft instances'),
   permission: Permission.Anyone,
-  allowInstance: false,
+  addMinecraftInstancesToOptions: OptionToAddMinecraftInstances.Disabled,
 
   handler: async function (context) {
     await context.interaction.deferReply()

src/instance/discord/commands/demote.ts
@@ -1,7 +1,7 @@
 import { escapeMarkdown, SlashCommandBuilder } from 'discord.js'
 
 import type { DiscordCommandHandler } from '../../../common/commands.js'
-import { Permission } from '../../../common/commands.js'
+import { OptionToAddMinecraftInstances, Permission } from '../../../common/commands.js'
 import { checkChatTriggers, formatChatTriggerResponse, RankChat } from '../common/chat-triggers.js'
 
 export default {
@@ -13,7 +13,7 @@ export default {
         option.setName('username').setDescription('Username of the player').setRequired(true).setAutocomplete(true)
       ) as SlashCommandBuilder,
   permission: Permission.Helper,
-  allowInstance: false,
+  addMinecraftInstancesToOptions: OptionToAddMinecraftInstances.Disabled,
 
   handler: async function (context) {
     await context.interaction.deferReply()

src/instance/discord/commands/invite.ts
@@ -1,7 +1,7 @@
 import { escapeMarkdown, SlashCommandBuilder } from 'discord.js'
 
 import type { DiscordCommandHandler } from '../../../common/commands.js'
-import { Permission } from '../../../common/commands.js'
+import { OptionToAddMinecraftInstances, Permission } from '../../../common/commands.js'
 import { checkChatTriggers, formatChatTriggerResponse, InviteAcceptChat } from '../common/chat-triggers.js'
 
 export default {
@@ -12,7 +12,7 @@ export default {
       .addStringOption((option) =>
         option.setName('username').setDescription('Username of the player').setRequired(true).setAutocomplete(true)
       ) as SlashCommandBuilder,
-  allowInstance: true,
+  addMinecraftInstancesToOptions: OptionToAddMinecraftInstances.Required,
 
   permission: Permission.Helper,
   handler: async function (context) {
@@ -21,7 +21,7 @@ export default {
     const username: string = context.interaction.options.getString('username', true)
     const command = `/g invite ${username}`
 
-    const instance: string | undefined = context.interaction.options.getString('instance') ?? undefined
+    const instance: string = context.interaction.options.getString('instance', true)
     const result = await checkChatTriggers(
       context.application,
       context.eventHelper,

src/instance/discord/commands/list.ts
@@ -8,7 +8,7 @@ import type Application from '../../../application.js'
 import type { MinecraftRawChatEvent } from '../../../common/application-event.js'
 import { Color, InstanceType } from '../../../common/application-event.js'
 import type { DiscordCommandHandler } from '../../../common/commands.js'
-import { Permission } from '../../../common/commands.js'
+import { OptionToAddMinecraftInstances, Permission } from '../../../common/commands.js'
 import type EventHelper from '../../../util/event-helper.js'
 import type { MojangApi, MojangProfile } from '../../../util/mojang.js'
 import { DefaultCommandFooter } from '../common/discord-config.js'
@@ -64,7 +64,7 @@ function createEmbed(instances: Map<string, string[]>): APIEmbed[] {
 export default {
   getCommandBuilder: () => new SlashCommandBuilder().setName('list').setDescription('List Online Players'),
   permission: Permission.Anyone,
-  allowInstance: false,
+  addMinecraftInstancesToOptions: OptionToAddMinecraftInstances.Disabled,
 
   handler: async function (context) {
     await context.interaction.deferReply()

commit c03dc6259e296ce78c315807226cb62739f51505

fix(discord): commands not registering due to invalid body
required options have to be first before optional ones. Update 53baa9c5e4a352c2f853de9df62758ddf48b2eb2 broke it

src/instance/discord/command-manager.ts
@@ -310,34 +310,48 @@ export class CommandManager extends EventHandler<DiscordInstance, InstanceType.D
         value: choice
       }))
 
+    /*
+    options are added after converting to json. 
+    This is done to specifically insert the "instance" option directly after the required options
+    the official api doesn't support this. So JSON manipulation is used instead.
+    This is mainly used for "Required" option. 
+    Discord will throw an error with "invalid body" otherwise.
+     */
     for (const command of this.commands.values()) {
-      const commandBuilder = command.getCommandBuilder()
+      const commandBuilder = command.getCommandBuilder().toJSON()
       const instanceCommandName = 'instance'
       const instanceCommandDescription = 'Which instance to send this command to'
+
       if (instanceChoices.length > 0) {
+        const index = commandBuilder.options?.findIndex((option) => option.required) ?? -1
+
         switch (command.addMinecraftInstancesToOptions) {
           case OptionToAddMinecraftInstances.Required: {
-            commandBuilder.addStringOption((option) =>
-              option
-                .setName(instanceCommandName)
-                .setDescription(instanceCommandDescription)
-                .setChoices(...instanceChoices)
-                .setRequired(true)
-            )
+            if (commandBuilder.options === undefined) commandBuilder.options = []
+
+            // splice is just fancy push at certain index
+            commandBuilder.options.splice(index + 1, 0, {
+              type: 3,
+              name: instanceCommandName,
+              description: instanceCommandDescription,
+              choices: instanceChoices,
+              required: true
+            })
             break
           }
           case OptionToAddMinecraftInstances.Optional: {
-            commandBuilder.addStringOption((option) =>
-              option
-                .setName(instanceCommandName)
-                .setDescription(instanceCommandDescription)
-                .setChoices(...instanceChoices)
-            )
+            if (commandBuilder.options === undefined) commandBuilder.options = []
+            commandBuilder.options.push({
+              type: 3,
+              name: instanceCommandName,
+              description: instanceCommandDescription,
+              choices: instanceChoices
+            })
           }
         }
       }
 
-      commandsJson.push(commandBuilder.toJSON())
+      commandsJson.push(commandBuilder)
     }
 
     return commandsJson