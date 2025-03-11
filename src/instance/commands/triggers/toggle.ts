4b9e142498962280c6c950f7a5a1a94381c6310e
aidn5 <aidn.david3@gmail.com>
2025-03-11 17:25:42 +0000

    fix(commands): !toggle command does not work in officer chat
    commands instance will default to helper permission by default to account for all staff if they execute something from officer chat.

--- src/instance/commands/triggers/toggle.ts
+++ src/instance/commands/triggers/toggle.ts
@@ -12,7 +12,7 @@ export default class Toggle extends ChatCommandHandler {
   }
 
   handler(context: ChatCommandContext): string {
-    if (context.permission !== Permission.Officer && context.permission !== Permission.Admin) {
+    if (context.permission < Permission.Helper) {
       return `${context.username}, Command can only be executed in officer chat or by the bridge admin`
     }
 
