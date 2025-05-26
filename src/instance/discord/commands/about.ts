790194d15eb273d5359f61e41cef13a32b72f3f9
aidn5 <aydin.atrakchi@gmail.com>
2025-05-26 06:53:56 +0200

    chore(discord): remove unnecessary defer in /about command

--- src/instance/discord/commands/about.ts
+++ src/instance/discord/commands/about.ts
@@ -11,9 +11,7 @@ export default {
   addMinecraftInstancesToOptions: OptionToAddMinecraftInstances.Disabled,
 
   handler: async function (context) {
-    await context.interaction.deferReply()
-
-    await context.interaction.editReply(
+    await context.interaction.reply(
       "This is a bridge that connects guild's discord server with it's guild chat while offering many features.\n" +
         'That way, you can chat with your friends within in-game from the discord.\n' +
         'The features of the bot can be viewed with /help.'
