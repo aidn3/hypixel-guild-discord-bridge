0d471030f26a9552a0a3ce0e6ee1738639474545
aidn5 <aydin.atrakchi@gmail.com>
2025-05-28 16:49:44 +0200

    lint(minecraft): lint send-queue file

--- src/instance/minecraft/common/send-queue.ts
+++ src/instance/minecraft/common/send-queue.ts
@@ -31,13 +31,15 @@ export class SendQueue {
     private readonly errorHandler: UnexpectedErrorHandler,
     private readonly sender: (command: string) => void
   ) {
-    void new Promise(async () => {
-      try {
-        await this.startCycle()
-      } catch (error: unknown) {
-        errorHandler.promiseCatch('queuing commands via minecraft instance')(error)
-      }
-    })
+    void Promise.resolve()
+      .then(async () => {
+        try {
+          await this.startCycle()
+        } catch (error: unknown) {
+          errorHandler.promiseCatch('queuing commands via minecraft instance')(error)
+        }
+      })
+      .catch(errorHandler.promiseCatch('sending queue loop'))
   }
 
   public notifyChatEvent(channel: ChannelType, message: string): void {
