f5e6ecd6ac35e6aa529e30a4d433ebbf5e5edd65
aidn5 <development@aidn5.com>
2025-12-30 09:18:51 +0000

    fix(commands): improve !bestiary mobs search

--- src/instance/commands/triggers/bestiary.ts
+++ src/instance/commands/triggers/bestiary.ts
@@ -1,7 +1,13 @@
+import assert from 'node:assert'
+
+import DefaultAxios from 'axios'
+import type { Logger } from 'log4js'
+import PromiseQueue from 'promise-queue'
+
 import type { ChatCommandContext } from '../../../common/commands.js'
 import { ChatCommandHandler } from '../../../common/commands.js'
+import Duration from '../../../utility/duration'
 import {
-  capitalize,
   getSelectedSkyblockProfileRaw,
   getUuidIfExists,
   playerNeverPlayedSkyblock,
@@ -9,6 +15,15 @@ import {
 } from '../common/utility'
 
 export default class Bestiary extends ChatCommandHandler {
+  private static readonly MaxLife = Duration.hours(6)
+  private static readonly Url =
+    'https://raw.githubusercontent.com/NotEnoughUpdates/NotEnoughUpdates-REPO/refs/heads/master/constants/bestiary.json'
+
+  private readonly singletonPromise = new PromiseQueue(1)
+  private fetchedAt = -1
+
+  private result: Record<string, string[]> | undefined
+
   constructor() {
     super({
       triggers: ['be', 'bestiary'],
@@ -19,7 +34,10 @@ export default class Bestiary extends ChatCommandHandler {
 
   async handler(context: ChatCommandContext): Promise<string> {
     const givenUsername = context.args[0] ?? context.username
-    const bestiaryName = context.args.at(1)
+    const bestiaryName = context.args
+      .slice(1)
+      .filter((part) => part.trim())
+      .join(' ')
 
     const uuid = await getUuidIfExists(context.app.mojangApi, givenUsername)
     if (uuid == undefined) return usernameNotExists(context, givenUsername)
@@ -35,28 +53,28 @@ export default class Bestiary extends ChatCommandHandler {
         ? 'never claimed bestiary milestones on this profile.'
         : `claimed ${bestiary.milestone.last_claimed_milestone} bestiary milestones.`
 
-    if (bestiaryName !== undefined) {
-      const acceptedNames = new Map<string, number>()
-      for (const [name, count] of Object.entries(bestiary.kills)) {
-        if (name === 'last_killed_mob' || typeof count !== 'number') continue
+    if (bestiaryName.length > 0) {
+      await this.singletonPromise.add(() => this.tryUpdate(context.logger))
+      assert.ok(this.result !== undefined)
 
-        const nameNormalized = name
-          .replaceAll(/_\d+$/g, '')
-          .split('_')
-          .map((part) => capitalize(part))
-          .map((part) => part.trim())
-          .filter((part) => part.length > 0)
-          .join(' ')
+      const resultList = new Map<string, number>()
+      for (const [name, idList] of Object.entries(this.result)) {
+        if (!name.toLowerCase().includes(bestiaryName.toLowerCase())) continue
 
-        if (!nameNormalized.toLowerCase().includes(bestiaryName.toLowerCase())) continue
-        const acceptedValue = acceptedNames.get(nameNormalized) ?? 0
-        acceptedNames.set(nameNormalized, acceptedValue + count)
+        const valueSoFar = resultList.get(name) ?? 0
+        let additionalValue = 0
+        for (const id of idList) {
+          const subCount = bestiary.kills[id]
+          if (typeof subCount === 'number') additionalValue += subCount
+        }
+
+        resultList.set(name, valueSoFar + additionalValue)
       }
 
-      if (acceptedNames.size === 0) {
+      if (resultList.size === 0) {
         return `${givenUsername} has never killed anything like that on this profile.`
       } else {
-        const entries = acceptedNames
+        const entries = resultList
           .entries()
           .toArray()
           .toSorted(([, a], [, b]) => b - a)
@@ -68,4 +86,51 @@ export default class Bestiary extends ChatCommandHandler {
 
     return response
   }
+
+  private async tryUpdate(logger: Logger): Promise<void> {
+    if (this.fetchedAt + Bestiary.MaxLife.toMilliseconds() < Date.now()) {
+      this.result = {}
+      const response = await DefaultAxios.get<NeuBestiary>(Bestiary.Url).then((response) => response.data)
+
+      const categoriesToCheck = Object.entries(response)
+        .filter(([key]) => key !== 'brackets')
+        .flatMap(([, category]) => Object.entries(category))
+
+      // Although it should NEVER happen. It is still a good idea to assume a max allowed work when doing recursive operations.
+      // Iterations required as of writing this: 78
+      const MaxIterations = 10_000
+      let currentIteration = 0
+
+      while (categoriesToCheck.length > 0) {
+        assert.ok(currentIteration++ < MaxIterations)
+
+        const entry = categoriesToCheck.pop()
+        assert.ok(entry !== undefined)
+
+        const [key, category] = entry
+        if (key === 'mobs') {
+          for (const mob of category as MobCategory['mobs']) {
+            const name = mob.name.replaceAll(/§\w/g, '')
+            if (name in this.result) {
+              logger.warn(`Already detected name "${name}" for bestiary command??. Continuing anyways.`)
+            }
+
+            this.result[name] = [...(this.result[name] ?? []), ...mob.mobs]
+          }
+        } else if (key !== 'icon' && key !== 'hasSubcategories' && key !== 'name') {
+          categoriesToCheck.push(...Object.entries(category as Record<string, unknown>))
+        } else {
+          logger.warn(`Ignored key: ${key}`)
+        }
+      }
+
+      this.fetchedAt = Date.now()
+    }
+  }
+}
+
+type NeuBestiary = Record<string, Record<string, unknown>>
+
+interface MobCategory {
+  mobs: { name: string; mobs: string[] }[]
 }
