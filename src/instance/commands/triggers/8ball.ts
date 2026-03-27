e157fcf056dfd42953c5aaea1842ed17dd88ab82
Fusionist-0 <fusionplayz0000@gmail.com>
2026-03-27 01:44:59 -0300

    style(commands): move !8ball response

--- src/instance/commands/triggers/8ball.ts
+++ src/instance/commands/triggers/8ball.ts
@@ -7,29 +7,6 @@
 import type { ChatCommandContext } from '../../../common/commands.js'
 import { ChatCommandHandler } from '../../../common/commands.js'
 
-const Answers = [
-  'As I see it, yes.',
-  'Ask again later.',
-  'Better not tell you now.',
-  'Cannot predict now.',
-  'Concentrate and ask again.',
-  "Don't count on it.",
-  'It is certain.',
-  'It is decidedly so.',
-  'Most likely.',
-  'My reply is no.',
-  'My sources say no.',
-  'Outlook not so good.',
-  'Outlook good.',
-  'Reply hazy, try again.',
-  'Signs point to yes.',
-  'Very doubtful.',
-  'Without a doubt.',
-  'Yes.',
-  'Yes – definitely.',
-  'You may rely on it.'
-]
-
 export default class EightBallCommand extends ChatCommandHandler {
   constructor() {
     super({
@@ -40,6 +17,11 @@ export default class EightBallCommand extends ChatCommandHandler {
   }
 
   handler(context: ChatCommandContext): string {
-    return `${context.username}, ${Answers[Math.floor(Math.random() * Answers.length)]}`
+    const messages = context.app.i18n.t(($) => $['commands.8ball'], {
+      returnObjects: true,
+      username: context.username
+    })
+    const message = messages[Math.floor(Math.random() * messages.length)]
+    return message
   }
 }
