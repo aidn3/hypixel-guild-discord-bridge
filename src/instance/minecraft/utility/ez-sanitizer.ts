579ea2493029b506743950ac7a69d37eefcdc1a9
Fusionist-0 <fusionplayz0000@gmail.com>
2026-04-08 00:21:11 -0300

    style(minecraft): replace EZPZ sanitization text
    This uses the character Ζ, not Z

--- src/instance/minecraft/utility/ez-sanitizer.ts
+++ src/instance/minecraft/utility/ez-sanitizer.ts
@@ -2,7 +2,7 @@ export default class EzSanitizer {
   public process(message: string): string {
     const regex = /(?<!\w)ez(?!\w)/g
     message = message.replaceAll(regex, '_ez') // it is on purpose that this checks word boundary
-    message = message.replaceAll(/ezpz/gi, 'ezp_z') // and this one does NOT.
+    message = message.replaceAll(/ezpz/gi, 'EΖPΖ') // and this one does NOT.
     return message
   }
 }
