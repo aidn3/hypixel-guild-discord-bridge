cdbc45aa6e444df0975672769b67e26c0e0c738e
aidn5 <development@aidn5.com>
2026-07-30 19:31:50 +0000

    fix(commands): account for Skyblock player data to be undefined
    changes:
    - handle when skills api being disabled in !skills
    - account for an empty "player_data" field in the API response

--- src/instance/commands/triggers/skills.ts
+++ src/instance/commands/triggers/skills.ts
@@ -27,27 +27,31 @@ export default class Skills extends ChatCommandHandler {
 
     const profile = memberProfile.members[uuid]
 
+    if (!profile.player_data?.experience) {
+      return context.app.i18n.t(($) => $['commands.skills.api-disabled'], { username: givenUsername })
+    }
+
     const skillsResponse = await context.app.hypixelApi.getSkyblockSkills()
     const skills = skillsResponse.skills
     const totalSocialExperience = Object.values(memberProfile.members) // Social level is the sum of all coop members
-      .map((profile) => profile.player_data.experience?.SKILL_SOCIAL ?? 0)
+      .map((profile) => profile.player_data?.experience?.SKILL_SOCIAL ?? 0)
       .reduce((a, b) => a + b, 0)
 
     const farmingCap = profile.jacobs_contest?.perks?.farming_level_cap ?? 0
     const tamingCap = profile.pets_data?.pet_care?.pet_types_sacrificed?.length ?? 0
 
-    const farming = this.getLevel(skills.FARMING, profile.player_data.experience?.SKILL_FARMING ?? 0)
-    const mining = this.getLevel(skills.MINING, profile.player_data.experience?.SKILL_MINING ?? 0)
-    const combat = this.getLevel(skills.COMBAT, profile.player_data.experience?.SKILL_COMBAT ?? 0)
-    const foraging = this.getLevel(skills.FORAGING, profile.player_data.experience?.SKILL_FORAGING ?? 0)
-    const fishing = this.getLevel(skills.FISHING, profile.player_data.experience?.SKILL_FISHING ?? 0)
-    const enchanting = this.getLevel(skills.ENCHANTING, profile.player_data.experience?.SKILL_ENCHANTING ?? 0)
-    const alchemy = this.getLevel(skills.ALCHEMY, profile.player_data.experience?.SKILL_ALCHEMY ?? 0)
-    const carpentry = this.getLevel(skills.CARPENTRY, profile.player_data.experience?.SKILL_CARPENTRY ?? 0)
-    const runecrafting = this.getLevel(skills.RUNECRAFTING, profile.player_data.experience?.SKILL_RUNECRAFTING ?? 0)
+    const farming = this.getLevel(skills.FARMING, profile.player_data.experience.SKILL_FARMING ?? 0)
+    const mining = this.getLevel(skills.MINING, profile.player_data.experience.SKILL_MINING ?? 0)
+    const combat = this.getLevel(skills.COMBAT, profile.player_data.experience.SKILL_COMBAT ?? 0)
+    const foraging = this.getLevel(skills.FORAGING, profile.player_data.experience.SKILL_FORAGING ?? 0)
+    const fishing = this.getLevel(skills.FISHING, profile.player_data.experience.SKILL_FISHING ?? 0)
+    const enchanting = this.getLevel(skills.ENCHANTING, profile.player_data.experience.SKILL_ENCHANTING ?? 0)
+    const alchemy = this.getLevel(skills.ALCHEMY, profile.player_data.experience.SKILL_ALCHEMY ?? 0)
+    const carpentry = this.getLevel(skills.CARPENTRY, profile.player_data.experience.SKILL_CARPENTRY ?? 0)
+    const runecrafting = this.getLevel(skills.RUNECRAFTING, profile.player_data.experience.SKILL_RUNECRAFTING ?? 0)
     const social = this.getLevel(skills.SOCIAL, totalSocialExperience)
-    const taming = this.getLevel(skills.TAMING, profile.player_data.experience?.SKILL_TAMING ?? 0)
-    const hunting = this.getLevel(skills.HUNTING, profile.player_data.experience?.SKILL_HUNTING ?? 0)
+    const taming = this.getLevel(skills.TAMING, profile.player_data.experience.SKILL_TAMING ?? 0)
+    const hunting = this.getLevel(skills.HUNTING, profile.player_data.experience.SKILL_HUNTING ?? 0)
 
     const totalLevels =
       farming + mining + combat + foraging + fishing + enchanting + alchemy + carpentry + taming + hunting
